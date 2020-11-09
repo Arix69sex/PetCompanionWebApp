351e01cfcea89076c26a6af3d87d2cc68304797c
Arix69sex <arix69sex@gmail.com>
2020-11-08 21:58:19 -0500

    Base Project

---
+++ src/main/java/com/acme/petcompanion/PetcompanionApplication.java
@@ -0,0 +1,13 @@
+package com.acme.petcompanion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetcompanionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetcompanionApplication.class, args);
+    }
+
+}
